0e67ea80bc18fa8d43b4980cfe265f2f4866ca5f
Bava <loic.steinmetz@dvp-web.fr>
2019-11-26 18:12:14 +0100

    aj presentations

--- src/main/java/org/asinfo/app/Main.java
+++ src/main/java/org/asinfo/app/Main.java
@@ -2,6 +2,7 @@ package org.asinfo.app;
 
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
+import org.asinfo.app.bot.Bot;
 import org.asinfo.app.la_faille.LaFaille;
 import org.asinfo.app.ping_pong.PingPong;
 
@@ -20,6 +21,7 @@ public class Main {
         JDA jda = new JDABuilder(System.getenv("DISCORD_TOKEN")).build();
 
         // Montage des différents services :
+        new Bot(jda);
         new PingPong(jda);
         new LaFaille(jda);
     }
---
+++ src/main/java/org/asinfo/app/bot/Bot.java
@@ -0,0 +1,29 @@
+package org.asinfo.app.bot;
+
+import net.dv8tion.jda.api.JDA;
+import net.dv8tion.jda.api.events.message.guild.GuildMessageReceivedEvent;
+import net.dv8tion.jda.api.hooks.ListenerAdapter;
+
+public class Bot extends ListenerAdapter {
+
+    /**
+     * Initialise des fonctionnalités Bot.
+     * @param jda JDA créé dans Main (JDA)
+     */
+    public Bot(JDA jda){
+        jda.addEventListener(this);
+    }
+
+    /**
+     * Réponse automatique au message "!javabot" ; présentation.
+     * @param event Messages envoyés sur le serveur
+     */
+    @Override
+    public void onGuildMessageReceived(GuildMessageReceivedEvent event){
+        if (event.getAuthor().isBot()) return;
+        String message = event.getMessage().getContentRaw();
+        if (message.equals("!javabot")){
+            event.getChannel().sendMessage("Hello c'est moi, Javabot !").queue();
+        }
+    }
+}
