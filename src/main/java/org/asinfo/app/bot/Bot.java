c7aef6dc9407ffb3b092f4ace42327d6658996bc
Bava <loic.steinmetz@dvp-web.fr>
2019-12-17 11:31:47 +0100

    aj !help bot

--- src/main/java/org/asinfo/app/bot/Bot.java
+++ src/main/java/org/asinfo/app/bot/Bot.java
@@ -9,6 +9,8 @@ import net.dv8tion.jda.api.hooks.ListenerAdapter;
  */
 public class Bot extends ListenerAdapter {
 
+    private static final String COMMANDES = "**Général**\n`!javabot` : \n`!projet` : \n\n**La Faille**\n`!jfaille` : Rejoindre la Faille\n`!qfaille` : Quitter la faille\n`!lafaille` : Appeler les membres de la faille";
+
     /**
      * Initialise des fonctionnalités Bot.
      * @param jda JDA créé dans Main (JDA)
@@ -29,6 +31,8 @@ public class Bot extends ListenerAdapter {
             event.getChannel().sendMessage("Hello c'est moi, Javabot !").queue();
         } else if (message.equals("!projet")){
             event.getChannel().sendMessage("Contribuez ici au projet :\nhttps://github.com/AS-Info-IUT-NC/javabot").queue();
+        } else if (message.equals("!help")){
+            event.getChannel().sendMessage(COMMANDES).queue();
         }
     }
 }
